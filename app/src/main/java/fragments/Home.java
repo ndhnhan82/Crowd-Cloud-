88f886700cecb50c31d9ecaefd180019ee86df5f
brianbelley <101362901+brianbelley@users.noreply.github.com>
2023-12-01 20:03:46 -0500

    Added the volley implementation to use string request, Added fully functional current weather request, Modified the UI for the home fragment, Applied an api with the current settings (free, 60 calls for 1 minute, 5-day forecast for free), Adding string resource and adding ForeCast later,

--- app/src/main/java/fragments/Home.java
+++ app/src/main/java/fragments/Home.java
@@ -1,14 +1,29 @@
 package fragments;
-
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.lasalle.crowdcloud.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.text.DecimalFormat;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +40,19 @@ public class Home extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // Fragmented Assets
+    private AutoCompleteTextView edLocation;
+    private Button btnFinish;
+    private ScrollView svResults;
+    private TextView tvCityResult, tvTemperatureResult, tvHumidityResult, tvDescriptionResult, tvWindSpeedResult, tvCloudinessResult, tvPressureResult;
+
+    //API
+
+    private static final String api = "a2efb35666c4f4327b66b2a93870e256";
+    private static final String url = "https://api.openweathermap.org/data/2.5/weather";
+
+    DecimalFormat df = new DecimalFormat("#.##");
 
     public Home() {
         // Required empty public constructor
@@ -60,7 +88,106 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate( R.layout.fragment_home, container, false );
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        // Initialize UI components
+        edLocation = view.findViewById(R.id.edLocation);
+        btnFinish = view.findViewById(R.id.btnFinish);
+        svResults = view.findViewById(R.id.svResults);
+        tvCityResult = view.findViewById(R.id.tvCityResult);
+        tvTemperatureResult = view.findViewById(R.id.tvTemperatureResult);
+        tvHumidityResult = view.findViewById(R.id.tvHumidityResult);
+        tvDescriptionResult = view.findViewById(R.id.tvDescriptionResult);
+        tvWindSpeedResult = view.findViewById(R.id.tvWindSpeedResult);
+        tvCloudinessResult = view.findViewById(R.id.tvCloudinessResult);
+        tvPressureResult = view.findViewById(R.id.tvPressureResult);
+
+
+        // Set onClickListener for the Finish button
+        btnFinish.setOnClickListener(v -> {
+            // Get the entered location from the AutoCompleteTextView
+            String location = edLocation.getText().toString().toLowerCase().trim();
+
+            // Make API request using Volley
+            fetchWeatherData(location);
+        });
+
+        return view;
     }
+
+    private void fetchWeatherData(String location) {
+        // Construct the API URL
+        String apiUrl = url + "?q=" + location + "&appid=" + api;
+
+        // Create a StringRequest to make the API request
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, apiUrl,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Handle the JSON response
+                        handleWeatherResponse(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle errors
+                        Log.e("Volley Error", error.toString());
+                        Toast.makeText(getContext(), "Error fetching weather data", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        // Add the request to the RequestQueue
+        Volley.newRequestQueue(requireContext()).add(stringRequest);
+    }
+
+    private void handleWeatherResponse(String response) {
+        try {
+            // Parsing the JSON response
+            JSONObject jsonObject = new JSONObject(response);
+
+            // Extracting relevant information
+            String cityName = jsonObject.getString("name");
+            double temperature = jsonObject.getJSONObject("main").getDouble("temp");
+            int humidity = jsonObject.getJSONObject("main").getInt("humidity");
+            JSONArray weatherArray = jsonObject.getJSONArray("weather");
+            double windSpeed = jsonObject.getJSONObject("wind").getDouble("speed");
+            int cloudiness = jsonObject.getJSONObject("clouds").getInt("all");
+            int pressure = jsonObject.getJSONObject("main").getInt("pressure");
+
+
+
+            double tempCelsius = Math.round(temperature - 273.15);
+
+
+            // Updating UI with the extracted information
+            tvCityResult.setText(cityName);
+            tvTemperatureResult.setText(String.valueOf((int)tempCelsius + " °C"));
+            tvHumidityResult.setText(String.valueOf(humidity));
+            tvWindSpeedResult.setText(String.valueOf(windSpeed) + " kmph");
+            tvCloudinessResult.setText(String.valueOf(cloudiness) + " %");
+            tvPressureResult.setText(String.valueOf(pressure) + " mb");
+
+            try {
+                //extracting the description
+                if (weatherArray.length() > 0) {
+                    JSONObject weatherObject = weatherArray.getJSONObject(0);
+                    String description = weatherObject.getString("description");
+                    String main = weatherObject.getString("main");
+
+                    tvDescriptionResult.setText(main + ", " + description);
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
